767f9b85b7fcae3ad49f47f6b19883f273b63fe7
Jai Kumar Dewani <jai-kumar.dewani@zeiss.com>
2023-09-02 23:03:02 +0530

    Working 3x3 game without winning rules

--- src/App.js
+++ src/App.js
@@ -1,3 +1,61 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React, { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button
+      className="square"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  )
+}
+
+export default function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true);
+
+  function handleClick(index) {
+    if (squares[index]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (isXNext) {
+      nextSquares[index] = "X";
+    }
+    else {
+      nextSquares[index] = "0";
+    }
+    setIsXNext(!isXNext);
+    setSquares(nextSquares);
+  }
+
+
+  return (
+    <div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </div>
+  )
+}
+
+function calculateWinnder(squares) {
+  const lines = [
+  
+  ]
+
 }

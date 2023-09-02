a6c4d73f0a90ab76e9a04f29c4822c16ab182a7b
Jai Kumar Dewani <jai-kumar.dewani@zeiss.com>
2023-09-02 23:10:15 +0530

    Working game with winning logic

--- src/App.js
+++ src/App.js
@@ -16,6 +16,15 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(true);
 
+  const winner = calculateWinnder(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${(isXNext ? "X" : "O")}`;
+  }
+
+
   function handleClick(index) {
     if (squares[index]) {
       return;
@@ -34,6 +43,7 @@ export default function Board() {
 
   return (
     <div>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -55,7 +65,20 @@ export default function Board() {
 
 function calculateWinnder(squares) {
   const lines = [
-  
-  ]
-
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }

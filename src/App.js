ca0ca1e42abc2ac5fafbcaeca2d1f879ce6e822f
Jai Kumar Dewani <jai-kumar.dewani@zeiss.com>
2023-09-09 14:37:50 +0530

    Made it look pertty and added rules

--- src/App.js
+++ src/App.js
@@ -23,12 +23,12 @@ export default function Game() {
         status = `Next player: ${(isXNext ? "X" : "O")}`;
     }
 
-    status += ` Current Board: ${currentBoard}`;
+    // status += ` Current Board: ${currentBoard}`;
 
     function updateCurrentBoard(nextMoveIndex) {
         if (winners.current[nextMoveIndex]) {
             let currentPlayableBoardIndexes = winners.current.map((value, index) => value == null ? index : null);
-            let nextCurrentBoards = currentPlayableBoardIndexes.filter(x => x!=null);
+            let nextCurrentBoards = currentPlayableBoardIndexes.filter(x => x != null);
             console.log('currentPlayableBoardIndexes', currentPlayableBoardIndexes);
             console.log("nextCurrentBoards", nextCurrentBoards)
             setCurrentBoard(nextCurrentBoards);
@@ -54,7 +54,7 @@ export default function Game() {
 
         setGameState(newBoards);
         setIsXNext(!isXNext);
-        
+
         checkForBoardWin(newNextMoveBoard, nextMoveBoardIndex);
         updateCurrentBoard(nextMoveIndex);
     }
@@ -76,44 +76,64 @@ export default function Game() {
 
     return (
         <div className="game">
-            <div className="status">{status}</div>
+            <div className={winner ? "winner" : "status"}>{status}</div>
             <div className="game-board">
                 <div className="game-row">
                     <Board
                         squares={gameState[0]} onPlay={handlePlay}
-                        boardIndex={0} isCurrentBoard={currentBoard.includes(0)} winner={winner} />
+                        boardIndex={0} isCurrentBoardActve={currentBoard.includes(0)} winner={winners.current[0]} />
                     <Board
                         squares={gameState[1]} onPlay={handlePlay}
-                        boardIndex={1} isCurrentBoard={currentBoard.includes(1)} winner={winner} />
+                        boardIndex={1} isCurrentBoardActve={currentBoard.includes(1)} winner={winners.current[1]} />
                     <Board
                         squares={gameState[2]} onPlay={handlePlay}
-                        boardIndex={2} isCurrentBoard={currentBoard.includes(2)} winner={winner} />
+                        boardIndex={2} isCurrentBoardActve={currentBoard.includes(2)} winner={winners.current[2]} />
                 </div>
                 <div className="game-row">
                     <Board
                         squares={gameState[3]} onPlay={handlePlay}
-                        boardIndex={3} isCurrentBoard={currentBoard.includes(3)} winner={winner} />
+                        boardIndex={3} isCurrentBoardActve={currentBoard.includes(3)} winner={winners.current[3]} />
                     <Board
                         squares={gameState[4]} onPlay={handlePlay}
-                        boardIndex={4} isCurrentBoard={currentBoard.includes(4)} winner={winner} />
+                        boardIndex={4} isCurrentBoardActve={currentBoard.includes(4)} winner={winners.current[4]} />
                     <Board
                         squares={gameState[5]} onPlay={handlePlay}
-                        boardIndex={5} isCurrentBoard={currentBoard.includes(5)} winner={winner} />
+                        boardIndex={5} isCurrentBoardActve={currentBoard.includes(5)} winner={winners.current[5]} />
                 </div>
                 <div className="game-row">
                     <Board
                         squares={gameState[6]} onPlay={handlePlay}
-                        boardIndex={6} isCurrentBoard={currentBoard.includes(6)} winner={winner} />
+                        boardIndex={6} isCurrentBoardActve={currentBoard.includes(6)} winner={winners.current[6]} />
                     <Board
                         squares={gameState[7]} onPlay={handlePlay}
-                        boardIndex={7} isCurrentBoard={currentBoard.includes(7)} winner={winner} />
+                        boardIndex={7} isCurrentBoardActve={currentBoard.includes(7)} winner={winners.current[7]} />
                     <Board
                         squares={gameState[8]} onPlay={handlePlay}
-                        boardIndex={8} isCurrentBoard={currentBoard.includes(8)} winner={winner} />
+                        boardIndex={8} isCurrentBoardActve={currentBoard.includes(8)} winner={winners.current[8]} />
                 </div>
             </div>
             <div className="game-info">
                 <ol>{/*TODO*/}</ol>
+            </div>
+            <div className="game-rules">
+                <h3>How to Play?</h3>
+                <ul className="game-rules-section">
+                    <li>Just like in regular tic-tac-toe, the two players (X and O) take turns, starting with X.</li>
+                    <li>The game starts with X playing wherever they want in any of the 81 empty spots.</li>
+                    <li>Next the opponent plays, however they are forced to play in the small board indicated by the relative location of the previous move. <br/></li>
+                    <li>For example, if X plays in the top right square of a small (3 × 3) board, then O has to play in the small board located at the top right of the larger board.</li>
+                    <li>Playing any of the available spots decides in which small board the next player plays.</li>
+                </ul>                    
+                <ul className="game-rules-section">
+                    <li>If a move is played so that it is to win a small board by the rules of normal tic-tac-toe, then the entire small board is marked as won by the player in the larger board.</li>
+                    <li>Once a small board is won by a player or it is filled completely, no more moves may be played in that board.</li>
+                    <li>If a player is sent to such a board, then that player may play in any other board.</li>
+                </ul>
+
+                <h3>How to Win?</h3>
+                <ul className="game-rules-section">
+                    <li>Game play ends when either a player wins the larger board or there are no legal moves remaining, in which case the game is a draw</li>
+                </ul>
             </div>
         </div>
     )
--- src/Board.js
+++ src/Board.js
@@ -11,7 +11,7 @@ function Square({ value, onSquareClick, isActive }) {
     )
 }
 
-export default function Board({ squares, onPlay, boardIndex, isCurrentBoard, winner }) {
+export default function Board({ squares, onPlay, boardIndex, isCurrentBoardActve, winner }) {
 
     function handleClick(index) {
         if (squares[index]) {
@@ -23,19 +23,22 @@ export default function Board({ squares, onPlay, boardIndex, isCurrentBoard, win
     return (
         <div className="board">
             <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} isActive={isCurrentBoard} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} isActive={isCurrentBoard} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} isActive={isCurrentBoard} />
+                <Square value={squares[0]} onSquareClick={() => handleClick(0)} isActive={isCurrentBoardActve} />
+                <Square value={squares[1]} onSquareClick={() => handleClick(1)} isActive={isCurrentBoardActve} />
+                <Square value={squares[2]} onSquareClick={() => handleClick(2)} isActive={isCurrentBoardActve} />
             </div>
             <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} isActive={isCurrentBoard} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} isActive={isCurrentBoard} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} isActive={isCurrentBoard} />
+                <Square value={squares[3]} onSquareClick={() => handleClick(3)} isActive={isCurrentBoardActve} />
+                <Square value={squares[4]} onSquareClick={() => handleClick(4)} isActive={isCurrentBoardActve} />
+                <Square value={squares[5]} onSquareClick={() => handleClick(5)} isActive={isCurrentBoardActve} />
             </div>
             <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} isActive={isCurrentBoard} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} isActive={isCurrentBoard} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} isActive={isCurrentBoard} />
+                <Square value={squares[6]} onSquareClick={() => handleClick(6)} isActive={isCurrentBoardActve} />
+                <Square value={squares[7]} onSquareClick={() => handleClick(7)} isActive={isCurrentBoardActve} />
+                <Square value={squares[8]} onSquareClick={() => handleClick(8)} isActive={isCurrentBoardActve} />
+            </div>
+            <div className="result"> 
+                {winner ? winner : ""}
             </div>
         </div>
     )

dd98601585126d569ebdb5a231b4af7360cae72a
Jai Kumar Dewani <jai-kumar.dewani@zeiss.com>
2023-09-09 12:29:09 +0530

    Working, yaaay

--- src/App.js
+++ src/App.js
@@ -1,104 +1,121 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Board from "./Board.js";
 import calculateWinnder from "./util.js";
 
 export default function Game() {
-	const [isXNext, setIsXNext] = useState(true);
+    const [isXNext, setIsXNext] = useState(true);
     const [gameState, setGameState] = useState(Array.from({ length: 9 }, () => new Array(9).fill(null)));
-	const [winners, setWinners] = useState(Array(9).fill(null));
+    const winners = useRef(Array(9).fill(null));
     //Use [] to store all boards which can be active at once, 
     //and only send isActive check down the components
-    
-    const [currentBoard, setCurrentBoard] = useState(4);
+    //Initialize currentBoard
+    const [currentBoard, setCurrentBoard] = useState([4]);
 
-	const winner = calculateWinnder(winners);
-	let status;
-	if (winner) {
-		status = `Winner: ${winner}`;
-	} else {
-		status = `Next player: ${(isXNext ? "X" : "O")}`;
-	}
+    // useEffect(() => {
+    //     console.log('useEffect ran. count is: ', count);
+    //   }, [gameState]);
 
-	status += ` Current Board: ${currentBoard}`;
+    const winner = calculateWinnder(winners.current);
+    let status;
+    if (winner) {
+        status = `Winner: ${winner}`;
+    } else {
+        status = `Next player: ${(isXNext ? "X" : "O")}`;
+    }
 
-	function handlePlay(nextMoveBoardIndex, nextMoveIndex) {
-		if (winner) {
-			return;
-		}
-		
+    status += ` Current Board: ${currentBoard}`;
+
+    function updateCurrentBoard(nextMoveIndex) {
+        if (winners.current[nextMoveIndex]) {
+            let currentPlayableBoardIndexes = winners.current.map((value, index) => value == null ? index : null);
+            let nextCurrentBoards = currentPlayableBoardIndexes.filter(x => x!=null);
+            console.log('currentPlayableBoardIndexes', currentPlayableBoardIndexes);
+            console.log("nextCurrentBoards", nextCurrentBoards)
+            setCurrentBoard(nextCurrentBoards);
+        } else {
+            setCurrentBoard([nextMoveIndex]);
+        }
+    }
+
+
+
+    function handlePlay(nextMoveBoardIndex, nextMoveIndex) {
+        if (winner) {
+            return;
+        }
         console.log("currentBoard", currentBoard);
-		console.log("nextMoveIndex", nextMoveIndex);
-		// console.log("boards", boards);
+        console.log("nextMoveIndex", nextMoveIndex);
+        // console.log("boards", boards);
 
-		let newBoards = gameState.slice();
-		let newNextMoveBoard = newBoards[currentBoard].slice();
-		newNextMoveBoard[nextMoveIndex] = isXNext ? "X" : "O";
-		newBoards[currentBoard] = newNextMoveBoard;
+        let newBoards = gameState.slice();
+        let newNextMoveBoard = newBoards[nextMoveBoardIndex].slice();
+        newNextMoveBoard[nextMoveIndex] = isXNext ? "X" : "O";
+        newBoards[nextMoveBoardIndex] = newNextMoveBoard;
 
-		setGameState(newBoards);
-		setCurrentBoard(nextMoveIndex);
-		setIsXNext(!isXNext);
+        setGameState(newBoards);
+        setIsXNext(!isXNext);
+        
+        checkForBoardWin(newNextMoveBoard, nextMoveBoardIndex);
+        updateCurrentBoard(nextMoveIndex);
+    }
 
-		checkForBoardWin(newNextMoveBoard, nextMoveBoardIndex);
-	}
+    function checkForBoardWin(boardState, boardIndex) {
+        // let boardState = gameState[boardIndex];
+        // console.log(boardState);
+        let boardWinner = calculateWinnder(boardState);
+        if (boardWinner) {
+            let newWinners = winners.current.slice();
 
-	function checkForBoardWin(boardState, boardIndex) {
-		// let boardState = gameState[boardIndex];
-		// console.log(boardState);
-		let boardWinner = calculateWinnder(boardState);
-		if (boardWinner) {
-			let newWinners = winners.slice();
+            newWinners[boardIndex] = boardWinner;
+            console.log("boardWinner", boardWinner, boardIndex);
+            console.log("Winners: ", newWinners);
 
-			newWinners[boardIndex] = boardWinner;
-			console.log("boardWinner", boardWinner, boardIndex);
-			console.log("Winners: ", newWinners);
+            winners.current = newWinners;
+        }
+    }
 
-			setWinners(newWinners);
-		}
-	}
-
-	return (
-		<div className="game">
-			<div className="status">{status}</div>
-			<div className="game-board">
-				<div className="game-row">
-					<Board 
-                        squares={gameState[0]} onPlay={handlePlay} 
-                        boardIndex={0} isCurrentBoard={currentBoard === 0} winner={winner} />
-					<Board 
-                        squares={gameState[1]} onPlay={handlePlay} 
-                        boardIndex={1} isCurrentBoard={currentBoard === 1} winner={winner} />
-					<Board 
-                        squares={gameState[2]} onPlay={handlePlay} 
-                        boardIndex={2} isCurrentBoard={currentBoard === 2} winner={winner} />
-				</div>
-				<div className="game-row">
-					<Board 
-                        squares={gameState[3]} onPlay={handlePlay} 
-                        boardIndex={3} isCurrentBoard={currentBoard === 3} winner={winner} />
-					<Board 
-                        squares={gameState[4]} onPlay={handlePlay} 
-                        boardIndex={4} isCurrentBoard={currentBoard === 4} winner={winner} />
-					<Board 
-                        squares={gameState[5]} onPlay={handlePlay} 
-                        boardIndex={5} isCurrentBoard={currentBoard === 5} winner={winner} />
-				</div>
-				<div className="game-row">
-					<Board 
-                        squares={gameState[6]} onPlay={handlePlay} 
-                        boardIndex={6} isCurrentBoard={currentBoard === 6} winner={winner} />
-					<Board 
-                        squares={gameState[7]} onPlay={handlePlay} 
-                        boardIndex={7} isCurrentBoard={currentBoard === 7} winner={winner} />
-					<Board 
-                        squares={gameState[8]} onPlay={handlePlay} 
-                        boardIndex={8} isCurrentBoard={currentBoard === 8} winner={winner} />
-				</div>
-			</div>
-			<div className="game-info">
-				<ol>{/*TODO*/}</ol>
-			</div>
-		</div>
-	)
+    return (
+        <div className="game">
+            <div className="status">{status}</div>
+            <div className="game-board">
+                <div className="game-row">
+                    <Board
+                        squares={gameState[0]} onPlay={handlePlay}
+                        boardIndex={0} isCurrentBoard={currentBoard.includes(0)} winner={winner} />
+                    <Board
+                        squares={gameState[1]} onPlay={handlePlay}
+                        boardIndex={1} isCurrentBoard={currentBoard.includes(1)} winner={winner} />
+                    <Board
+                        squares={gameState[2]} onPlay={handlePlay}
+                        boardIndex={2} isCurrentBoard={currentBoard.includes(2)} winner={winner} />
+                </div>
+                <div className="game-row">
+                    <Board
+                        squares={gameState[3]} onPlay={handlePlay}
+                        boardIndex={3} isCurrentBoard={currentBoard.includes(3)} winner={winner} />
+                    <Board
+                        squares={gameState[4]} onPlay={handlePlay}
+                        boardIndex={4} isCurrentBoard={currentBoard.includes(4)} winner={winner} />
+                    <Board
+                        squares={gameState[5]} onPlay={handlePlay}
+                        boardIndex={5} isCurrentBoard={currentBoard.includes(5)} winner={winner} />
+                </div>
+                <div className="game-row">
+                    <Board
+                        squares={gameState[6]} onPlay={handlePlay}
+                        boardIndex={6} isCurrentBoard={currentBoard.includes(6)} winner={winner} />
+                    <Board
+                        squares={gameState[7]} onPlay={handlePlay}
+                        boardIndex={7} isCurrentBoard={currentBoard.includes(7)} winner={winner} />
+                    <Board
+                        squares={gameState[8]} onPlay={handlePlay}
+                        boardIndex={8} isCurrentBoard={currentBoard.includes(8)} winner={winner} />
+                </div>
+            </div>
+            <div className="game-info">
+                <ol>{/*TODO*/}</ol>
+            </div>
+        </div>
+    )
 }
 
--- src/Board.js
+++ src/Board.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 function Square({ value, onSquareClick, isActive }) {
     return (

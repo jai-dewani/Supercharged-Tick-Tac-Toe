cd2a5213707f4627f22072c6ec8aaa8df7f9a598
Jai Kumar Dewani <jai-kumar.dewani@zeiss.com>
2023-09-11 00:43:13 +0530

    Works on iPhones now, atleast

--- src/Board.js
+++ src/Board.js
@@ -4,7 +4,7 @@ function Square({ value, onSquareClick, isActive }) {
     return (
         <button
             className={isActive ? "activeSquare" : "square"}
-            onClick={isActive ? onSquareClick: null}
+            onClick={isActive ? onSquareClick : null}
         >
             {value}
         </button>
@@ -22,23 +22,26 @@ export default function Board({ squares, onPlay, boardIndex, isCurrentBoardActve
 
     return (
         <div className="board">
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} isActive={isCurrentBoardActve} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} isActive={isCurrentBoardActve} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} isActive={isCurrentBoardActve} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} isActive={isCurrentBoardActve} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} isActive={isCurrentBoardActve} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} isActive={isCurrentBoardActve} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} isActive={isCurrentBoardActve} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} isActive={isCurrentBoardActve} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} isActive={isCurrentBoardActve} />
-            </div>
-            <div className="result"> 
-                {winner ? winner : ""}
+            <div className="inner-board">
+
+                <div className="board-row">
+                    <Square value={squares[0]} onSquareClick={() => handleClick(0)} isActive={isCurrentBoardActve} />
+                    <Square value={squares[1]} onSquareClick={() => handleClick(1)} isActive={isCurrentBoardActve} />
+                    <Square value={squares[2]} onSquareClick={() => handleClick(2)} isActive={isCurrentBoardActve} />
+                </div>
+                <div className="board-row">
+                    <Square value={squares[3]} onSquareClick={() => handleClick(3)} isActive={isCurrentBoardActve} />
+                    <Square value={squares[4]} onSquareClick={() => handleClick(4)} isActive={isCurrentBoardActve} />
+                    <Square value={squares[5]} onSquareClick={() => handleClick(5)} isActive={isCurrentBoardActve} />
+                </div>
+                <div className="board-row">
+                    <Square value={squares[6]} onSquareClick={() => handleClick(6)} isActive={isCurrentBoardActve} />
+                    <Square value={squares[7]} onSquareClick={() => handleClick(7)} isActive={isCurrentBoardActve} />
+                    <Square value={squares[8]} onSquareClick={() => handleClick(8)} isActive={isCurrentBoardActve} />
+                </div>
+                <div className="result">
+                    {winner ? winner : ""}
+                </div>
             </div>
         </div>
     )
